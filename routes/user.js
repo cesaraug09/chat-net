56c26bb68fd150fd882b841c3b859811f26d8057
cesar <cesar.caju@academico.ifpb.edu.br>
2024-10-02 19:01:34 -0300

    pequena orientação ao front

--- routes/user.js
+++ routes/user.js
@@ -14,9 +14,17 @@ router.get('/register',(req, res)=>{
 
 router.post('/authenticationreg',(req, res)=>{
     if(req.body.password == req.body.confirmpassword){
-        res.send(`Bem vindo, <b>${req.body.username}</b>!`)
+        res.send(`Bem vindo, <b>${req.body.username}</b>!<i>Essa tela não é um handlebars e não pode ser estilizada AINDA...</i>`)
     } else{
         res.render('authentication/register')
+    }
+})
+
+router.post('/authenticationlog', (req,res)=>{
+    if(req.body.password !='' && req.body.username!=""){
+        res.send(`Bem vindo, <b>${req.body.username}</b>! Sua senha eh ${req.body.password} kkkkk<i>Essa tela não é um handlebars e não pode ser estilizada AINDA...</i>`)
+    } else{
+        res.render('authentication/login')
     }
 })
 
