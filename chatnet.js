9600514d7ab07e35732c209867f14b75b11999d8
cesar <cesar.caju@academico.ifpb.edu.br>
2024-10-03 17:49:48 -0300

    instalando e configurando middlewars flash e session

--- chatnet.js
+++ chatnet.js
@@ -2,29 +2,47 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const mongoose = require('mongoose');
 const user = require('./routes/user')
-// Configurando express
 const app = express();
-app.engine('handlebars', handlebars.engine({
-    defaultLayout: 'main',
-    runtimeOptions:{
-        allowProtoMethodsByDefault: true,
-        allowedProtoMethodsByDefault: true,
-    }}))
-// Handlebars
-    app.set('view engine', 'handlebars');
-    app.use(express.urlencoded({extended: false}));
-    app.use(express.json());
-// Public
-    app.use(express.static('public'));
-// Mongoose
-    mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/chatnet').then(()=>{
-        console.log("Conectado ao MongoDB do chatnet ✔️\n")
-    }).catch((err)=>{
-        console.log(`Ocorreu um erro ao tentar se conectar ao banco de dados: ${err}`)
-    })
+const session = require('express-session');
+const flash = require("connect-flash");
+// Configurações
+    app.engine('handlebars', handlebars.engine({
+        defaultLayout: 'main',
+        runtimeOptions:{
+            allowProtoMethodsByDefault: true,
+            allowedProtoMethodsByDefault: true,
+        }}))
+    // Handlebars
+        app.set('view engine', 'handlebars');
+        app.use(express.urlencoded({extended: false}));
+        app.use(express.json());
+    // Public
+        app.use(express.static('public'));
+    // Mongoose
+        mongoose.Promise = global.Promise;
+        mongoose.connect('mongodb://localhost/chatnet').then(()=>{
+            console.log("Conectado ao MongoDB do chatnet ✔️\n")
+        }).catch((err)=>{
+            console.log(`Ocorreu um erro ao tentar se conectar ao banco de dados: ${err}`)
+        })
+    //Sessão
+        app.use(session({
+            secret: "202222",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    // MiddleWare
+        app.use((req,res,next)=>{
+            res.locals.sucess_msg = req.flash("sucess_msg") // sucess_msg e error_msg são variaveis globais
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })
 
 // Rotas:
+app.use((req,res,next)=>{
+    next();
+})
 
 app.get('/', function(req, res){
     res.render('splash/splash')
@@ -35,6 +53,7 @@ app.use('/user', user);
 
 
 // Geral
-app.listen('3000', function(){
+let PORT = "3001"
+app.listen(PORT, function(){
     console.log("\n>>>>>  O CHATNET está On-Line 🟢.  <<<<<\n")
 })
